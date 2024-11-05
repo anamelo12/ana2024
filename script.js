129a52e2797c8389a8e998edcccb3e5da4aa7273
anamelo12 <ana.garcia.melo@escola.pr.gov.br>
2024-11-05 11:41:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+<html>
+      <head>
+            <title>calculo da medida </title>
+      <head>
+        <body>
+           <h1> calculo da medida de 1 aluno </h1>
+           <input type: "number" max="100" id="nota-1"
+           placeholder="insira a nota 1">
+          <input type= "number" max ="100" id="nota_2"
+          placeholder="insira a nota 2">
+      <button id=calcule onglick="calcular()">
+       calcular
+       </button>
